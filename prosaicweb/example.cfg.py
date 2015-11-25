00382aa414d6a1c326025261ee87a16e04b76acc
nathaniel smith <nathanielksmith@gmail.com>
2015-11-25 14:41:13 -0800

    add example cfg

---
+++ prosaicweb/example.cfg.py
@@ -0,0 +1,17 @@
+# don't change probably
+SYSTEM_USER='system'
+
+# can be whatever
+SITE_NAME='prosaicweb'
+
+# name of mongodb db to use
+DBNAME = 'prosaicweb'
+
+# flask debug
+DEBUG = False
+
+# set to something real
+SECRET_KEY = 'skreeeeekettttttttt yo'
+
+# probably fine
+MAX_UPLOAD_SIZE = 5 * 1024 * 1024 # 5mb
